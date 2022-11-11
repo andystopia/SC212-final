264e0f08e93abb60c3f8b983afdcea902628be20
andystopia <91233633+andystopia@users.noreply.github.com>
2022-11-11 10:05:32 -0500

    cleaned lung cancer data, using relevant script

--- R Scripts/cancer-process.R
+++ R Scripts/cancer-process.R
@@ -4,10 +4,18 @@ male.cancer <- read.csv(file = file.choose())
 female.cancer <- read.csv(file = file.choose())
 
 not_all_na <- function(x) any(!is.na(x))
-male.cancer.simple <- male.cancer %>% dplyr::select(where(not_all_na))
-female.cancer.simple <- female.cancer %>% dplyr::select(where(not_all_na))
+process <- . %>%
+  dplyr::select(where(not_all_na)) %>%
+  distinct(ISO_3_CODE, .keep_all = T) %>%
+  select(-c(RANK, CNTRY_TERR)) %>%
+  filter(VALUE != "No data")
 
-joined <- inner_join(male.cancer.simple, female.cancer.simple, by=c("ISO_3_CODE"), suffix=c(".male", ".female"))%>%
-  select(-c("CNTRY_TERR.female"))
+male.cancer.simple <- male.cancer %>% process
+female.cancer.simple <- female.cancer %>% process
+
+
+male.cancer.simple %>% View
+female.cancer.simple %>% View
+joined <- inner_join(male.cancer.simple, female.cancer.simple, by=c("ISO_3_CODE"), suffix=c(".male", ".female"))
 
 write.csv(joined, file.choose(new=T), row.names=F)
--- R Scripts/join-tobacco.R
+++ R Scripts/join-tobacco.R
@@ -2,10 +2,11 @@ library(dplyr)
 
 dataset <- read.csv(file="../datasets/prelim-data-with-some-country-codes.csv")
 combined.cancer.com <- read.csv(file="../datasets/cancer-atlas-lung-cancer-combined.csv")
+combined.cancer.com.dis <- combined.cancer.com %>% distinct
 
 left_join(
   dataset,
-  combined.cancer.com %>%
+  combined.cancer.com.dis %>%
     rename(ALPHA3 = ISO_3_CODE) %>%
     select(-CNTRY_TERR.male), by=c("ALPHA3")
-  ) %>% write.csv(file=file.choose(new=T))
+  ) %>% View
